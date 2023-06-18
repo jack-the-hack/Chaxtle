335da16a9a1b34840e8ec2f2c392f22f66a00e8d
Jack Baldyga <50553957+jack-the-hack@users.noreply.github.com>
2023-06-18 14:07:34 -0400

    Update infograb.py

--- infograb.py
+++ infograb.py
@@ -27,3 +27,44 @@ def grabgtext(searchtype,search):
   pass
 def refergpt(searchtype,search):
   pass
+import itertools
+
+def test(numbers):
+    # Your implementation of the test function
+    # ...
+    pass
+
+def generate_input_list(example_list, max_value, step_size):
+    num_steps = int(max_value / step_size) + 1
+
+    for bits in itertools.product([0, 1], repeat=len(example_list)):
+        input_list = [round(example_list[i], 1) if bit else round(step_size * j, 1)
+                      for i, (bit, j) in enumerate(zip(bits, range(num_steps)))]
+        yield input_list
+
+def find_best_input_list(example_list):
+    max_ones = 0
+    best_input_list = []
+
+    max_value = 20
+    step_size = 0.1
+
+    for input_list in generate_input_list(example_list, max_value, step_size):
+        classification = test(input_list)
+        ones = classification.count(1)
+
+        if ones > max_ones:
+            max_ones = ones
+            best_input_list = input_list
+
+    return best_input_list
+
+# Example usage:
+example_list = [0.3, 5, 0.3, 0.5, 8, 0.005, 0.008, 0.010, 10, 2]
+best_input = find_best_input_list(example_list)
+classification = test(best_input)
+
+print("Example List:", example_list)
+print("Best Input List:", best_input)
+print("Classification:", classification)
+print("Number of 1s:", classification.count(1))
